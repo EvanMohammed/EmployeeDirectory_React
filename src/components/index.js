76aba29d10eac09b25009b85f363ffdf58cf0598
Evan <evanmohammedsaleh@gmail.com>
2020-09-17 23:17:13 -0500

    added initial styling

--- src/components/index.js
+++ src/components/index.js
@@ -1,35 +1,44 @@
 import React, {useState, useEffect} from "react";
-import API from "../utils/API"
-
+import API from "../utils/API";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import "../components/style.css"
 
 function Employees () {
     const [employeeSearch , setEmployeeSearch] = useState({
-        name: ""
-        
+        first: "",
+        last: ""
     })
     useEffect(() => {
         API.getUsers().then((res) => {
             setEmployeeSearch(res.data)
-            console.log(employeeSearch)
-            console.log(employeeSearch.results[100].name.first)
+            // console.log(employeeSearch)
+            // console.log(employeeSearch.results)
+            // console.log(employeeSearch.results)
+            console.log(employeeSearch.results)
         })
-    }, [employeeSearch]);
+    });
 
 
     return (
-        <div className="card">
-      <div>
-        Name: {employeeSearch.name}
-      </div>
-
-      <div className="btn-group">
-        <button onClick={() => setEmployeeSearch({ ...employeeSearch, name: "lazy" })} className="btn btn-danger">
-          Encourage Laziness
-        </button>
-            </div>
-
-
+        <div className="container">
+      <div className="jumbotron jumbotron-fluid" id="jumbotron">
+  <div className="container">
+    <h1 className="display-4">Employee Data</h1>
+    <p className="lead">PLease Type the Employee Name to Search</p>
+  </div>
 </div>
+          
+          
+          <form>
+  <div className="form-group">
+    
+    <input type="text" className="form-control" id="exampleInputEmail1"  placeholder="Employee Name" />
+    <button type="button" className="btn btn-success" id="searchBtn">Search</button>
+  </div>
+        </form>
+</div>
+
+
 
 
 
