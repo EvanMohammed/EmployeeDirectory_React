ac095f321a5ea935467d56f34fdeae2ee276b6da
Evan <evanmohammedsaleh@gmail.com>
2020-09-18 08:45:25 -0500

    API Calls work

--- src/components/index.js
+++ src/components/index.js
@@ -1,42 +1,62 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import API from "../utils/API";
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import "../components/style.css"
 
-function Employees () {
-    const [employeeSearch , setEmployeeSearch] = useState({
-        first: "",
-        last: ""
+function Employees() {
+  const [employeeSearch, setEmployeeSearch] = useState({
+    filterUsers: [],
+    users: []
+  })
+  useEffect(() => {
+    API.getUsers().then((res) => {
+      console.log(res.data.results[0])
+      // setEmployeeSearch(res.data)
+      // console.log(employeeSearch)
+      // console.log(employeeSearch.results)
+      // console.log(employeeSearch.results)
+      // console.log(employeeSearch.results);
+      setEmployeeSearch({
+        ...employeeSearch,
+        users: res.data.results,
+        filterUsers: res.data.results
+
+      })
     })
-    useEffect(() => {
-        API.getUsers().then((res) => {
-            setEmployeeSearch(res.data)
-            // console.log(employeeSearch)
-            // console.log(employeeSearch.results)
-            // console.log(employeeSearch.results)
-            console.log(employeeSearch.results)
-        })
-    });
+  }, []);
 
 
-    return (
-        <div className="container">
-      <div className="jumbotron jumbotron-fluid" id="jumbotron">
-  <div className="container">
-    <h1 className="display-4">Employee Data</h1>
-    <p className="lead">PLease Type the Employee Name to Search</p>
-  </div>
-</div>
+  return (
+   
+            <div className="container">
+         <img src="https://jgparker.com/wp-content/uploads/2016/01/Employee-Benefits-5.jpg" class="img-fluid" alt="Responsive image" /> 
+      
+   
+
+
+              <form>
+      <div className="form-group">
+
+        <input type="text" className="form-control" id="exampleInputEmail1"  placeholder="Employee Name" />
+        <button type="button" className="btn btn-success" id="searchBtn">Search</button>
+      </div>
+            </form>
+
+
+            <ul>
+      {employeeSearch.users.map((user, i) =>
+        (<div className="card" >
+
+          <li key={user.id.value}>{user.dob.date}</li>
+          <li key={user.id.value}>{user.email}</li>
+          <li key={user.id.value }>{user.phone}</li>
+          <li key={user.id.value }>{user.image}</li>
           
-          
-          <form>
-  <div className="form-group">
-    
-    <input type="text" className="form-control" id="exampleInputEmail1"  placeholder="Employee Name" />
-    <button type="button" className="btn btn-success" id="searchBtn">Search</button>
-  </div>
-        </form>
-</div>
+
+
+        </div>)
+      )}
+    </ul>
+    </div>
 
 
 
@@ -45,7 +65,7 @@ function Employees () {
 
 
 
-    )
+  )
 
 }
 
