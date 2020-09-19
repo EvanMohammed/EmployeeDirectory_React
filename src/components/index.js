f92e1a708c2ea6fc9a27c1478d0d792fa65b4875
Evan <evanmohammedsaleh@gmail.com>
2020-09-19 00:09:33 -0500

    Added a Search.js file

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
 import "../components/style.css"
+import SearchForm from "../components/searchForm/SearchForm";
 
 function Employees() {
   const [employeeSearch, setEmployeeSearch] = useState({
@@ -10,11 +11,7 @@ function Employees() {
   useEffect(() => {
     API.getUsers().then((res) => {
       console.log(res.data.results[0])
-      // setEmployeeSearch(res.data)
-      // console.log(employeeSearch)
-      // console.log(employeeSearch.results)
-      // console.log(employeeSearch.results)
-      // console.log(employeeSearch.results);
+    
       setEmployeeSearch({
         ...employeeSearch,
         users: res.data.results,
@@ -23,6 +20,15 @@ function Employees() {
       })
     })
   }, []);
+  const handleInputChange = event => {
+    setEmployeeSearch(event.target.value);
+  };
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setEmployeeSearch(event.target.search.value);
+    console.log(employeeSearch.state.search);
+    console.log(employeeSearch.state.result);
+  };
 
 
   return (
@@ -33,29 +39,31 @@ function Employees() {
    
 
 
-              <form>
-      <div className="form-group">
-
-        <input type="text" className="form-control" id="exampleInputEmail1"  placeholder="Employee Name" />
-        <button type="button" className="btn btn-success" id="searchBtn">Search</button>
-      </div>
-            </form>
+         
+            <card heading="Search">
+              <SearchForm
+                
+                handleInputChange={handleInputChange}
+                handleFormSubmit={handleFormSubmit}
+              />
+          
 
 
             <ul>
-      {employeeSearch.users.map((user, i) =>
+      {employeeSearch.users.map((user) =>
         (<div className="card" >
 
           <li key={user.id.value}>{user.dob.date}</li>
           <li key={user.id.value}>{user.email}</li>
           <li key={user.id.value }>{user.phone}</li>
-          <li key={user.id.value }>{user.image}</li>
+          <li key={user.id.value }> {user.picture.medium}</li>
           
 
 
         </div>)
       )}
     </ul>
+      </card>
     </div>
 
 
